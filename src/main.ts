9230d3a6eeb5d558343d0f22625fb2b5a9562f1c
Lucas Lima <lucasteofilo.contact@gmail.com>
2025-04-03 13:59:14 -0300

    deploy cors

--- src/main.ts
+++ src/main.ts
@@ -12,21 +12,42 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    // allowedHeaders: ['content-type'],
-    // methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
-    // preflightContinue: true,
     credentials: true,
-    origin: [
-      // 'http://localhost',
-      // 'capacitor://localhost',
-      // 'ionic://localhost',
-      // 'http://localhost',
-      // 'http://localhost:3001',
-      // 'http://localhost:3100',
-      // 'http://localhost:4200',
-      'https://ativar-garantia-services-ativar-garantia-admin.dxwvrh.easypanel.host',
-      'https://gestao.ativargarantia.com.br'
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost',
+        'capacitor://localhost',
+        'ionic://localhost',
+        'http://localhost:3001',
+        'http://localhost:3100',
+        'http://localhost:4200',
+        'https://ativar-garantia-services-ativar-garantia-admin.dxwvrh.easypanel.host',
+        'https://gestao.ativargarantia.com.br',
+        'https://ativargarantia.com.br'
+      ];
+      
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      
+      return callback(new Error('Not allowed by CORS'));
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Referer',
+      'User-Agent',
+      'sec-ch-ua',
+      'sec-ch-ua-mobile',
+      'sec-ch-ua-platform'
     ],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
   
   await app.listen(3000);
